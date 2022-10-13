d57c185fe1dd9f065d0cf08c21ec5801d4f8d672
jongbub <bub3690@naver.com>
2022-10-13 20:00:08 +0900

    FEAT: Update deeplabv3 Plus

---
+++ custom/deeplabv3plus_r50/deeplabv3plus_r50-d8_4x4_512x512_80k_NoPmd.py
@@ -0,0 +1,8 @@
+_base_ = [
+    './deeplabv3plus_r50-d8.py',
+    '../base/bmc_dataset_512x512_NoPMD.py',
+    '../base/default_runtime.py',
+    '../base/schedule_80k.py'
+]
+model = dict(
+    decode_head=dict(num_classes=6), auxiliary_head=dict(num_classes=6))
